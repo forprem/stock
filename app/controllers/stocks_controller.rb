4cb3f49f58c07441c4ddbeb00fde58f96e27d1c0
Prem <upsei@ups.com>
2018-12-01 03:53:16 +0000

    Ajax Added

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,18 @@
 class StocksController < ApplicationController
     def search
-       @stock = SStock.new_from_lookup(params[:stock])
-       render "users/my_portfolio"
-       #render json: @stock
+        if params[:stock].present?
+            @stock = SStock.new_from_lookup(params[:stock])
+            if @stock
+                render partial: 'users/result'
+                #render "users/my_portfolio"
+                #render json: @stock
+            else
+                flash.now[:danger] = "You have Entered Incorrect Ticker"
+                render partial: 'users/result'
+            end
+        else
+            flash.now[:danger] = "Please enter Ticker value"
+            render partial: 'users/result'
+        end
     end
 end
--- app/models/s_stock.rb
+++ app/models/s_stock.rb
@@ -1,11 +1,17 @@
 class SStock < ActiveRecord::Base
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
     def self.new_from_lookup(ticker_symbol)
-        looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-       # price = strip_commas(looked_up_stock.latest_price)
-        new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+        begin
+            looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+            # price = strip_commas(looked_up_stock.latest_price)
+            new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+        rescue Exception => e
+            return nil
+        end
     end
     
-    def self.strip_commas(number)
-        number.gsub("," "")
-    end
+#    def self.strip_commas(number)
+#        number.gsub("," "")
+#    end
 end
